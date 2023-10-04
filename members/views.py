b8c5d8bd0a922e80b2908945395d417ecb2dfc9a
vasyl <vasyl.savuliak@outlook.com>
2023-10-04 23:52:48 +0200

    index page added

--- members/views.py
+++ members/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
+def home(request):
+    template  = loader.get_template('home.html')
+    return HttpResponse(template.render())
 def members(request):
     template  = loader.get_template('home.html')
     return HttpResponse(template.render())
